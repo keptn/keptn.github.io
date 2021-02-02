67a9163ae99d462f6286a19f2d5a189fc91f67ea
nicoblak <nico@fredmansky.at>
2021-02-02 13:04:57 +0100

    Update happy-users-logos script random.
    Signed-off-by: nicoblak <nico@fredmansky.at>

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,28 +7,8 @@ var logo = document.querySelector('.logo');
 var logoJS = document.querySelector('.logo-js');
 var logoIntro = document.querySelector('.logo-intro');
 var hamburgerInner = document.querySelector('.hamburger-inner');
-
-window.addEventListener('DOMContentLoaded', function () {
-  new Splide('.splide', {
-    perPage: 2,
-    perMove: 1,
-    height: '380px',
-    focus: 'center',
-    drag: true,
-    type: 'loop',
-    breakpoints: {
-      992: {
-        perPage: 1,
-        drag: true,
-        type: 'loop',
-        padding: {
-          right: '1.5rem',
-          left: '1.5rem',
-        },
-      },
-    },
-  }).mount();
-});
+var happyUserLogos = document.querySelectorAll('.js-happy-users-logo');
+var numberOfLogos = 6;
 
 if(body.classList.contains('page-home')) {
   window.addEventListener('scroll', (event) => {
@@ -48,8 +28,50 @@ if(body.classList.contains('page-home')) {
       logoJS.src = '/images/home/hero/keptn-logo-white.svg';
     }
   });
+
+  window.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+      perPage: 2,
+      perMove: 1,
+      height: '380px',
+      focus: 'center',
+      drag: true,
+      type: 'loop',
+      breakpoints: {
+        992: {
+          perPage: 1,
+          drag: true,
+          type: 'loop',
+          padding: {
+            right: '1.5rem',
+            left: '1.5rem',
+          },
+        },
+      },
+    }).mount();
+
+    for (var randomLogoNumbers = [], i = 0; i < numberOfLogos; ++i) randomLogoNumbers[i] = i+1;
+    shuffle(randomLogoNumbers);
+    var counter = 0;
+    happyUserLogos.forEach(logo => {
+      logo.src = '/images/home/happy-users/logos/logo-' + randomLogoNumbers[counter] +  '.png';
+      counter = ++counter;
+    });
+  });
 } else {
   logoJS.src = '/images/logo.svg';
+}
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
 
 if (menuTrigger) {
