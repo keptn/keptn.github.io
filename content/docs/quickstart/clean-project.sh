adb921ba2155327b34557365f12fab2c36edec60
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2021-06-02 10:57:51 +0200

    refactoring
    Signed-off-by: jetzlstorfer <juergen.etzlstorfer@dynatrace.com>

--- content/docs/quickstart/clean-project.sh
+++ content/docs/quickstart/clean-project.sh
@@ -3,8 +3,8 @@ set -e
 
 keptn delete project podtatohead
 rm -rf podtato-head
-kubectl delete deploy -n keptn prometheus-service
-kubectl delete deploy -n keptn prometheus-sli-service
+kubectl delete deploy -n keptn prometheus-service --ignore-not-found=true
+kubectl delete deploy -n keptn prometheus-sli-service --ignore-not-found=true
 kubectl delete secret -n keptn prometheus-credentials-podtatohead  --ignore-not-found=true
 kubectl delete ns monitoring --ignore-not-found=true
 
